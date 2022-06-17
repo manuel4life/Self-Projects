8a227a94e6f04a23738508b1f72703b303a20e2b
manuel4life <allotey4eva@gmail.com>
2022-06-17 20:20:24 +0000

    just wrote the last printf statment to print the Element to stdout

--- 0x01/1-array.c
+++ 0x01/1-array.c
@@ -10,6 +10,11 @@ int main{void}
 	printf("Element at %d is %d\n", 2, number[2]);
 	printf("Element at %d is %d\n", 3, number[3]);
 	printf("Element at %d is %d\n", 4, number[4]);
+	printf("Element at %d is %d\n", 5, number[5]);
+	printf("Element at %d is %d\n", 6, number[6]);
+	printf("Element at %d is %d\n", 7, number[7]);
+	printf("Element at %d is %d\n", 8, number[8]);
+	printf("Element at %d is %d\n", 9, number[9]);
 
 	return (0);
 }
