700dd0add5a5bce6cb98615f6ea58ce879f11fc8
manuel4life <allotey4eva@gmail.com>
2022-06-17 20:17:50 +0000

    the 5th printf statement to print the index

--- 0x01/1-array.c
+++ 0x01/1-array.c
@@ -6,6 +6,10 @@ int main{void}
 	int number[10] = {20, 30, 40, 50, 60, 70, 80, 90, 100, 10};
 
 	printf("Element at %d is %d\n", 0, number[0]);
+	printf("Element at %d is %d\n", 1, number[1]);
+	printf("Element at %d is %d\n", 2, number[2]);
+	printf("Element at %d is %d\n", 3, number[3]);
+	printf("Element at %d is %d\n", 4, number[4]);
 
 	return (0);
 }
