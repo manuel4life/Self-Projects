85a1e010374a468d3c6b4adca85d55e4288f12a4
manuel4life <allotey4eva@gmail.com>
2022-06-17 22:22:43 +0000

    corrections of code

--- 0x02/2-array.c
+++ 0x02/2-array.c
@@ -1,19 +1,30 @@
 #include <stdio.h>
 
-int (main)
+int main(void)
 {
 	float salary[10];
 	
-	salary[0];
-	salary[1];
-	salary[2];
-	salary[3];
-	salary[4];
-	salary[5];
-	salary[6];
-	salary[7];
-	salary[8];
-	salary[9];
+	salary[0] = 200.23;
+	salary[1] = 190.87;
+	salary[2] = 2009.40;
+	salary[3] = 90098;
+	salary[4] = 90887;
+	salary[5] = 654378;
+	salary[6] = 546342;
+	salary[7] = 645343;
+	salary[8] = 3224;
+	salary[9] = 45322;
+
+	printf("Element at %d is %0.2f\n", 0, salary[0]);
+	printf("Element at %d is %0.2f\n", 1, salary[1]);
+	printf("Element at %d is %0.2f\n", 2, salary[2]);
+	printf("Element at %d is %0.2f\n", 3, salary[3]);
+	printf("Element at %d is %0.2f\n", 4, salary[4]);
+	printf("Element at %d is %0.2f\n", 5, salary[5]);
+	printf("Element at %d is %0.2f\n", 6, salary[6]);
+	printf("Element at %d is %0.2f\n", 7, salary[7]);
+	printf("Element at %d is %0.2f\n", 8, salary[8]);
+	printf("Element at %d is %0.2f\n", 9, salary[9]);
 	
 
 	return (0);
