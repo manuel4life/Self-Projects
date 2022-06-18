1a5551e8bb8625d8609313eb9122057052fbbee8
manuel4life <allotey4eva@gmail.com>
2022-06-18 16:23:12 +0000

    another for loop to check the index,and if the condtion is true,we should print the index

--- 0x03/position.c
+++ 0x03/position.c
@@ -3,10 +3,19 @@
 int main(void)
 {
 	unsigned int SIZE = 20;
-	int n [SIZE];
+	int n[SIZE];
 	int position, index;
 
 	for (position = 0; position < SIZE; position++)
+	{
+		n[position] = position + 5;
+	}
+
+	for (index = 0; index < SIZE; index++)
+	{
+		printf("The Element at index %d is", index);
+		printf("%d\n", n[index]);
+	}
 
 	return (0);
 }
