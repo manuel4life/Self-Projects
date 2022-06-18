6e03dcf9b9e81d3f42f8afc21a9131de4233a4f0
manuel4life <allotey4eva@gmail.com>
2022-06-18 17:11:36 +0000

    using for loop to initialize i and doing a condition on it

--- 0x04/total.c
+++ 0x04/total.c
@@ -7,5 +7,12 @@ int main(void)
 	int date[TOTAL-SIZE];
 	int total = 0, i, j, product;
 
+	for (i = 0; i < TOTAL-SIZE; i++)
+	{
+		product = i * TOTAL-SIZE;
+		date[i] = product;
+		total = total + product;
+	}
+
 	return (0);
 }
